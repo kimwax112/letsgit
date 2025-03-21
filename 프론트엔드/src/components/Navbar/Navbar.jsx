6ed4572aa79eafc2eb812cc2eb2b3eceb3937a10
marong113 <sooujng113@gmail.com>
2025-03-21 22:29:20 +0900

    내비게이션바 드롭다운 수정

--- 프론트엔드/src/components/Navbar/Navbar.jsx
+++ 프론트엔드/src/components/Navbar/Navbar.jsx
@@ -29,10 +29,59 @@ const Navbar = () => {
 
         {/* 기본 메뉴 */}
         <div className="nav-links">
-          <a href="#">직접 의류 디자인</a>
-          <a href="#">제작 의뢰 맡기기</a>
-          <a href="#">계약 관리</a>
-          <a href="#">대화방</a>
+          <div className="dropdown">
+            <a href="#">직접 의류 디자인</a>
+            <div className="dropdown-menu">
+              <Link to="/clothes" onClick={() => setIsMenuOpen(false)}>
+                사이트 제공 템플릿
+              </Link>
+              <Link to="/Upload" onClick={() => setIsMenuOpen(false)}>
+                디자인 파일 업로드
+              </Link>
+              <Link to="/BrandDP" onClick={() => setIsMenuOpen(false)}>
+                브랜드 디자인 검색
+              </Link>
+            </div>
+          </div>
+          <div className="dropdown">
+            <a href="#">제작 의뢰 맡기기</a>
+            <div className="dropdown-menu">
+              <Link to="/request" onClick={() => setIsMenuOpen(false)}>
+                의뢰 등록하기
+              </Link>
+              <Link to="/designer" onClick={() => setIsMenuOpen(false)}>
+                디자이너 고르기
+              </Link>
+            </div>
+          </div>
+          <div className="dropdown">
+            <a href="#">계약 관리</a>
+            <div className="dropdown-menu">
+              <Link to="/contract" onClick={() => setIsMenuOpen(false)}>
+                계약서 조회 및 처리
+              </Link>
+              <Link to="/modify" onClick={() => setIsMenuOpen(false)}>
+                계약 수정 건의
+              </Link>
+              <Link to="/cancel" onClick={() => setIsMenuOpen(false)}>
+                계약 해지 요청
+              </Link>
+              <Link to="/refund" onClick={() => setIsMenuOpen(false)}>
+                환불 요청
+              </Link>
+            </div>
+          </div>
+          <div className="dropdown">
+            <a href="#">대화방</a>
+            <div className="dropdown-menu">
+              <Link to="/chat" onClick={() => setIsMenuOpen(false)}>
+                일반 채팅방
+              </Link>
+              <Link to="/report" onClick={() => setIsMenuOpen(false)}>
+                사용자 신고/차단
+              </Link>
+            </div>
+          </div>
         </div>
       </nav>
 
@@ -59,9 +108,6 @@ const Navbar = () => {
             <Link to="/designer" onClick={() => setIsMenuOpen(false)}>
               디자이너 고르기
             </Link>
-            <Link to="/recruit" onClick={() => setIsMenuOpen(false)}>
-              디자이너 구인 게시판
-            </Link>
           </div>
           <div className="dropdown-section">
             <h3>계약 관리</h3>
@@ -85,6 +131,24 @@ const Navbar = () => {
             </Link>
             <Link to="/report" onClick={() => setIsMenuOpen(false)}>
               사용자 신고/차단
+            </Link>
+          </div>
+          <div className="dropdown-section">
+            <h3>마이페이지</h3>
+            <Link to="" onClick={() => setIsMenuOpen(false)}>
+              내 정보
+            </Link>
+            <Link to="" onClick={() => setIsMenuOpen(false)}>
+              디자인&의뢰
+            </Link>
+            <Link to="" onClick={() => setIsMenuOpen(false)}>
+              진행내역 조회
+            </Link>
+            <Link to="" onClick={() => setIsMenuOpen(false)}>
+              작성한 후기
+            </Link>
+            <Link to="" onClick={() => setIsMenuOpen(false)}>
+              찜한 디자이너
             </Link>
           </div>
         </div>
