81d0a0d39d031406b56a603a33ad072c27ac232c
marong113 <sooujng113@gmail.com>
2025-03-21 22:42:18 +0900

    위로 스크롤 버튼 구현

--- 프론트엔드/src/App.js
+++ 프론트엔드/src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Header, Footer, Navbar } from "./components"
 import RouterComponent from "./RouterComponent"// 라우터 관리 파일 분리
+import ScrollToTopBtn from "./components/ScrollToTopBtn/ScrollToTopBtn";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Navbar />
         <RouterComponent /> {/* 라우팅 담당 컴포넌트 */}
+        <ScrollToTopBtn/>
         <Footer />
       </Router>
     </div>
