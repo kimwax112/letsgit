dceb6906c7bab984a0b63c7fd3e087fd1c5427d1
marong113 <sooujng113@gmail.com>
2025-03-25 21:15:47 +0900

    디자이너페이지 헤더와 내비게이션바 수정

--- 프론트엔드/src/DesignerLayout.jsx
+++ 프론트엔드/src/DesignerLayout.jsx
@@ -1,10 +1,13 @@
-import {Header,Navbar,ScrollToTopBtn,Footer} from './components';
+import {ScrollToTopBtn,Footer} from './components';
+import DesignerHeader from './components/DesignerHeader/DesignerHeader';
+import DesignerNavbar from './components/DesignerNavbar/DesignerNavbar';
+
 
 export default function DesignerLayout({children}) {
   return (
     <>
-    
-    
+    <DesignerHeader/>
+    <DesignerNavbar/>
     {children}
     <ScrollToTopBtn/>
     <Footer />
