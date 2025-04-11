be195123fe7c326a89e7b381c597489bc34e8c4c
marong113 <sooujng113@gmail.com>
2025-04-12 07:49:24 +0900

    의뢰인마이페이지 내정보 ui구현

--- 프론트엔드/src/pages/MyPage/FavoriteDesigners/FavoriteDesignersPage.jsx
+++ 프론트엔드/src/pages/MyPage/FavoriteDesigners/FavoriteDesignersPage.jsx
@@ -2,10 +2,15 @@
 import React from "react";
 import ClientMypageLayout from "../../../layouts/ClientMypageLayout";
 import FavoriteDesigners from "../../../components/Mypage/FavoriteDesigners/FavoriteDesigners";
+import { Heart } from "lucide-react";
 
 const FavoriteDesignersPage = () => {
     return (
-        <ClientMypageLayout>
+        <ClientMypageLayout
+        title="찜한 디자이너"
+        breadcrumb="마이페이지 > 찜한 디자이너"
+        icon={Heart}
+        >
             <FavoriteDesigners />
         </ClientMypageLayout>
     );
