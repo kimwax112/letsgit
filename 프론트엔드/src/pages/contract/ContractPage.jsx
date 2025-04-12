23de58122c83ae39d869fbc285112afaff5dfe8a
marong113 <sooujng113@gmail.com>
2025-04-12 19:42:56 +0900

    의뢰인 계약관리 검색창 수정

--- 프론트엔드/src/pages/contract/ContractPage.jsx
+++ 프론트엔드/src/pages/contract/ContractPage.jsx
@@ -1,12 +1,11 @@
 import ContractLayout from "../../layouts/ContractLayout";
 import ContractSearchAndFilter from "../../components/contract/ContractSearchAndFilter/ContractSearchAndFilter";
-import ContractList from "../../components/contract/ContractList";
+import ContractList from "../../components/contract/ContractList/ContractList";
 
 const ContractPage = () => {
   return (
     <ContractLayout>
       <div className="p-6">
-        <ContractSearchAndFilter />
         <ContractList />
       </div>
     </ContractLayout>
--- 프론트엔드/src/pages/contract/StarredPage.jsx
+++ 프론트엔드/src/pages/contract/StarredPage.jsx
@@ -1,12 +1,11 @@
 import ContractLayout from "../../layouts/ContractLayout";
 import ContractSearchAndFilter from "../../components/contract/ContractSearchAndFilter/ContractSearchAndFilter";
-import ContractList from "../../components/contract/ContractList";
+import ContractList from "../../components/contract/ContractList/ContractList";
 
 const StarredPage = () => {
   return (
     <ContractLayout>
       <div className="p-6">
-        <ContractSearchAndFilter />
         {/* 중요 계약들만 표시하는 리스트 */}
         <ContractList />
       </div>
