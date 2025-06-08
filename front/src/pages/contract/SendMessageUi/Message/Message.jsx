d1c7d6ed46aed973ad6a1e79deff1e4734c53b25
Kimsejeong04 <129528881+Kimsejeong04@users.noreply.github.com>
2025-06-08 19:56:27 +0900

    병합

--- front/src/pages/contract/SendMessageUi/Message/Message.jsx
+++ front/src/pages/contract/SendMessageUi/Message/Message.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Message.css";
@@ -39,32 +38,15 @@ export default function Message() {
 
   const handleClick = () => {
     // 상세 페이지로 이동
-=======
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./Message.css";
-
-export default function Message({ id, contract, content, time, designer }) {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
->>>>>>> feature/sj
     navigate(`/client/contract/MessageDetailpage/${id}`);
   };
 
   return (
     <div className="message-container" onClick={handleClick}>
-<<<<<<< HEAD
       <div className="contract-title">{contract.contractTitle}</div>
       <div className="design-name">{contract.designerid || "디자이너 없음"}</div>
       <div className="contract-detail">{content || "보낸 메시지 미리보기"}</div>
       <div className="contract-date">{time || contract.date || "날짜 없음"}</div>
-=======
-      <div className="contract-title">{contract || "계약 정보 없음"}</div>
-       <div className="design-name">{designer || "디자이너 정보 없음"}</div>
-      <div className="contract-detail">{content || "보낸 메시지 미리보기"}</div>
-      <div className="contract-date">{time || "날짜 없음"}</div>
->>>>>>> feature/sj
     </div>
   );
 }
