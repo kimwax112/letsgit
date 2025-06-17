779d7ae32f4f7bdbb468aeb22b82ba4e6e87bdcf
marong113 <sooujng113@gmail.com>
2025-06-17 22:18:18 +0900

    수정

--- PjBackend2/src/main/java/com/example/Loginpj/controller/UsingSessionPrac.java
+++ PjBackend2/src/main/java/com/example/Loginpj/controller/UsingSessionPrac.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 import java.nio.file.Files;
